f6a5e8e2d27cdc8765075dfd843cebda35a80527
turrs <faturrahmanavoiders@gmail.com>
2022-12-23 15:46:51 +0800

    remove babel eslint

--- src/parts/Headers/index.js
+++ src/parts/Headers/index.js
@@ -39,14 +39,6 @@ const Headers = () => {
                 >
                   Home
                 </Link>
-              </li>{" "}
-              <li>
-                <Link
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  to="/"
-                >
-                  Home
-                </Link>
               </li>
             </ul>
           </nav>
